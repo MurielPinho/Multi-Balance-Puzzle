18fb88d31cffe2bd32bc642b3a6d3905e380c868
André Mamprin Mori <andremori16@gmail.com>
2020-12-30 17:27:16 -0300

    Update p.pl

--- src/p.pl
+++ src/p.pl
@@ -1,8 +1,13 @@
 :- consult('board.pl').
 :- consult('display.pl').
 :- consult('utilities.pl').
+?- use_module(library(clpfd)).
 :- use_module(library(lists)).
 :- use_module(library(random)).
+
+% reconsult file
+r :-
+    reconsult('p.pl').
 
 play :-
     generateRandomBoard(B1),
